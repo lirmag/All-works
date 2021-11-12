e2f16e835a7d79b74ad19fdc5d17deb72a0b45c0
lirmag <isaev1lev@gmail.com>
2021-11-12 12:44:10 +0300

    classwork

--- classwork6.py
+++ classwork6.py
@@ -3,16 +3,15 @@ import random
 
 for i in range(1, 11):
     f = open(str(i) + 'txt', 'w+')
-f = open(str(random.randint(1, 10)) + 'txt','w+')
+    f.close()
+f = open(str(random.randint(1, 10)) + 'txt', 'w+')
 f.write('Hello')
 f.close()
-for i in range(1,11):
+for i in range(1, 11):
     s = open(str(i) + 'txt', 'r')
-    s.readlines()
     if 'Hello' in s:
         continue
-    else:
-        os.remove(str(i) + 'txt')
     s.close()
-# for i in range(1,11):
-#     os.remove(str(i) + 'txt')
+    os.remove(str(i) + 'txt')
+
+# os.remove(str(3) + 'txt')
